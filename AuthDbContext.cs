97e0cb7238f767c3d134b6e5dce2cbb640d1df26
Sylvester Roshan <135866192+Cric24@users.noreply.github.com>
2024-07-15 13:42:22 +0530

    Add files via upload

---
+++ AuthDbContext.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.EntityFrameworkCore;
+using BookStore.Models;
+
+namespace BookStore
+{
+    public class AuthDbContext : DbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+
+        }
+
+        // DbSet for the Book entity
+        public DbSet<BookDetails> BookDetails { get; set; }
+
+
+
+        // DbSet for the Customer entity
+        public DbSet<Customer> Customers { get; set; }
+
+        // DbSet for the Order entity
+        public DbSet<Order> Orders { get; set; }
+
+
+        // DbSet for the Customer Login entity
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<Cart> Cart { get; set; }
+
+
+        // DbSet for the Feedback entity with table name Feedback
+        public DbSet<Feedback> Feedback { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+using BookStore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+// Configure authentication
+builder.Services.AddAuthentication("CookieAuth")
+    .AddCookie("CookieAuth", config =>
+    {
+        config.Cookie.Name = "App.Cookie"; 
+        config.LoginPath = "/Account/Login"; 
+    });
+
+
+
+
+
+// Add database context
+builder.Services.AddDbContext<AuthDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. if we want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Add authentication middleware
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
